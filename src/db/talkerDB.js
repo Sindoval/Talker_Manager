2ea69c47b7efa5d852e4fe3b781387584e4426b5
Sindoval <sindoval321@gmail.com>
2025-05-24 20:35:13 -0300

    Rota get/talker/:id concluído

--- src/db/talkerDB.js
+++ src/db/talkerDB.js
@@ -2,6 +2,9 @@ const connection = require('./connection');
 
 const findAll = () => connection.execute(`SELECT * FROM talkermanagerdb.talkers;`);
 
+const findById = (id) => connection.execute(`SELECT * FROM talkermanagerdb.talkers WHERE id = ?;`, [id]);
+
 module.exports = {
   findAll,
+  findById,
 };
--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { findAll } = require('../db/talkerDB');
+const { findAll, findById } = require('../db/talkerDB');
 
 const router = express.Router();
 
@@ -10,6 +10,28 @@ router.get('/', async (_req, res) => {
       return res.status(200).json([]);
     }
     return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.sqlMessage });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    console.log({ id: id });
+    const [[result]] = await findById(Number(id));
+    console.log({ api: result });
+
+    if (!result) {
+      return res.status(404).json({ message: "Pessoa palestrante não encontrada" })
+    }
+    return res.status(200).json({
+      id: result.id,
+      name: result.name,
+      age: result.age,
+      talk: { watchedAt: result.talk_watched_at, rate: result.talk_rate }
+    });
   } catch (error) {
     return res.status(500).json({ message: error.sqlMessage });
   }
